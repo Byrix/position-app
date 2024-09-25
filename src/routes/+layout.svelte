f508acfcd56b980cce14651a974f301b2095f8bb
Sean <28848951+Byrix@users.noreply.github.com>
2024-09-25 16:29:14 +1000

    added home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,29 +10,34 @@
 
 <!-- Header -->
 <!-- HTML <a href="{base}/"></a> tag for routing -->
-<div class="navbar bg-base-300">
+<div class="navbar bg-base-300 flex flex-row">
     <a
-        class="btn btn-ghost text-xl"
+        class="btn btn-ghost text-xl flex-grow ml-5"
         href="{base}/"
     >
         Home
     </a>
     <a
-        class="btn btn-ghost text-xl"
+        class="btn btn-ghost text-xl flex-grow"
         href="{base}/about"
     >
         About
     </a>
     <a
-        class="btn btn-ghost text-xl"
+        class="btn btn-ghost text-xl flex-grow"
         href="{base}/example">
         Example
+    </a>
+    <a
+        class="btn btn-ghost text-xl ml-auto mr-5"
+        href="{base}/play">
+        Play
     </a>
 </div>
 
 <!-- Main content -->
 <!-- Content is inserted at <slot /> -->
 <!-- DoNotChange -->
-<main class="max-w-[1920px] container mx-auto pt-4">
+<main class="max-w-[1920px] container mx-auto h-screen">
     <slot reset />
 </main>
