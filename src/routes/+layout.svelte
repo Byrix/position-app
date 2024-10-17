65f76d69eb244887f87a59d744bfdfa6f4ac38dd
Sean <28848951+Byrix@users.noreply.github.com>
2024-10-17 20:04:03 +1100

    automatic dark mode (ish)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,28 @@
 <script>
     import { base } from '$app/paths' // DoNotChange
-    import { getCookie } from '$lib'
+    import { getCookie, setCookie, toTitleCase } from '$lib'
+    import { onMount } from 'svelte'
     import '../app.css' // DoNotChange
 
     let nsfw, nsfwPrompt
     nsfw = getCookie(nsfw)
     if (!nsfw) { nsfwPrompt = true }
+
+    let dark
+    onMount(async () => {
+        dark = await getCookie(dark)
+        if (typeof dark == 'undefined') {
+            dark = await (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
+            setCookie('dark', dark, 10)
+        }
+    })
+
+    const navOptions = {
+        home: '/',
+        about: '/about',
+        example: '/example',
+        play: '/play'
+    }
 </script>
 
 <!-- Website title -->
@@ -15,35 +32,21 @@
 
 <!-- Header -->
 <!-- HTML <a href="{base}/"></a> tag for routing -->
-<div class="navbar bg-base-300 flex flex-row">
-    <a
-        class="btn btn-ghost text-xl flex-grow ml-5"
-        href="{base}/"
-    >
-        Home
-    </a>
-    <a
-        class="btn btn-ghost text-xl flex-grow"
-        href="{base}/about"
-    >
-        About
-    </a>
-    <a
-        class="btn btn-ghost text-xl flex-grow"
-        href="{base}/example">
-        Example
-    </a>
-    <a
-        class="btn btn-ghost text-xl ml-auto mr-5"
-        href="{base}/play">
-        Play
-    </a>
+<div class="navbar bg-surface0 flex flex-row {dark ? 'latte' : 'macchiato'}">
+    {#each Object.entries(navOptions) as [name, path]}
+        <a
+            class="btn btn-ghost text-xl flex-grow text-text"
+            href="{base}{path}"
+        >
+            {toTitleCase(name)}
+        </a>
+    {/each}
 </div>
 
 <!-- Main content -->
 <!-- Content is inserted at <slot /> -->
 <!-- DoNotChange -->
-<main class="max-w-[1920px] container mx-auto h-screen">
+<main class="max-w-[1920px] container mx-auto h-screen {dark ? 'latte' : 'macchiato'}">
     <!-- {#if nsfwPrompt}
         <div class="max-w-[50%] max-h-[50%] bg-peach z-1">
             NSFW Prompt here!
