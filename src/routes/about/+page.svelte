48fa2d3ed2e4f436aa3842760ccb8991ca605a80
lbannan <leannebannan@gmail.com>
2024-10-18 14:04:09 +1100

    icons

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,14 @@
+<script>
+    import Art from '$lib/assets/art.svelte'
+</script>
+
 <div class="w-full text-center">
     <p>This is an about page</p>
+    <p>
+        I am testing my icon
+        <span style="display: inline-block; vertical-align: middle;">
+            <Art />
+        </span>
+        to see if I can print it inline
+    </p>
 </div>
