abd0915e2a6fba52a6f41ef7cd87bb40834a0b5d
lbannan <leannebannan@gmail.com>
2024-10-20 16:03:03 +1100

    about page and missing icons

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,22 +1,48 @@
-<script>
-    import Art from '$lib/assets/art.svelte'
-    import Beat from '$lib/assets/beat.svelte'
-    import Church from '$lib/assets/church.svelte'
-</script>
+<style>
+    a {
+        text-decoration: underline;
+        font-weight: bold;
+    }
+</style>
 
 <div class="w-full text-center">
-    <p>This is an about page</p>
     <p>
-        I am testing my icon
-        <span style="display: inline-block; vertical-align: middle;">
-            <Art />
-        </span>
-        to see if I can print it inline!
-        <span style="display: inline-block; vertical-align: middle;">
-            <Beat />
-        </span>
-        <span style="display: inline-block; vertical-align: middle;">
-            <Church />
-        </span>
-    </p>
+        New to Melbourne and feeling a bit lonely? Looking for community?
+        Or just interested in learning more about our city's queer history?
+        PridePoints is the game for you! A GNSS game designed to help you explore
+        Melbourne through a new lens while interacting with others in the LGBTQIA+ community.<br><br>
+
+        <strong><em>How to play</em></strong><br>
+        Click on the locator button and start walking towards the icons on the map. Each icon is an important place
+        of queer history in Melbourne. When you arrive at the locations, a pop-up will tell you more. <br><br>
+        But what's that? Didn't we call this a game? Click 'Play' under the information and start a game with other PridePoints
+        players who have visited the same locations. Every time you complete a game, you will earn points and make your own
+        mark on the map. Slowly build up your own queer history through interactions with others in the community. <br><br>
+        <strong><em>Queer Quests</em></strong><br>
+        Don't feel like playing with others? That's okay! You can still earn points by visiting locations and
+        completing Queer Quests. These include GNSS enabled walking tours, collaborative story telling, contributing
+        to digital community artworks and more. Build up a repuation for yourself on the leaderboard or play anonymously.
+        It's your choice! We are just here to support you on your journey, whatever that looks like for you. <br><br>
+
+        <strong><em>Content warning</em></strong><br>
+        We believe that Melbourne is for everyone, but that has not always been the case.
+        Homosexuality was illegal in our city until 1982 and until then, queer folks were forced to
+        live, meet and express themselves away from the public eye. This means that some of our locations are
+        places of violence and discrimination. If you find this triggering, use the 'NSFW' button to
+        filter out sexually explicit or upsetting content. Notice something we didn't catch? Press report to help us
+        keep PridePoints safe for everyone.<br><br>
+
+        <em><strong>Acknowledgements</strong></em><br>
+        This game is a celebration of progress and a testament to those who protested,
+        partied and pushed to change Melbourne into the inclusive city that it is today. PridePoints is a GNSS game adaptation of
+        <a href="https://www.queerways.au/"> Queer-ways by LUCIANO and Georgia Keats</a>.
+        Created in 2021, Queer-ways is a Melbourne-based collaborative community arts
+        project that actualises the queer cartography of Australia by placing its history
+        onto interactive, digital maps. The information and data used by PridePoints
+        has been downloaded from Queer-ways and is a combination of information from the
+        <a href="https://queerarchives.org.au/">Australian Queer Archives (AQuA)</a> and community contributions.<br><br>
+
+        PridePoints is based on the stolen lands of the Wurundjeri Woi Wurrung people of the
+        Kulin Nations. We pay respect to Traditional Owners, Elders and Knowledge Holders of all
+        Indigenous nations and acknowledge the ongoing history of sexual and gender diversity represented within these cultures.</p>
 </div>
