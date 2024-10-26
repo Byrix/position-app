cdeedbd0728beb0196b7045dcdcb63aa68c3988b
Sean <28848951+Byrix@users.noreply.github.com>
2024-10-26 20:45:07 +1100

    Updates

---
+++ _app/immutable/entry/start.UGwhNUmg.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CDtiK05J.js";export{t as start};
---
+++ _app/immutable/nodes/3.BqRUUUbj.js
@@ -0,0 +1,27 @@
+import{s as n,e as s,c as l,h as u,i as h,j as c,n as o,g as d}from"../chunks/scheduler.OQqrjfzc.js";import{S as p,i as m}from"../chunks/index.B3-jCvVV.js";function y(r){let e,a=`<p>New to Melbourne? Looking for community?
+        Or just interested in learning more about our city&#39;s queer history?
+        PridePoints is the game for you! A GNSS game designed to help you explore
+        Melbourne through a new lens while interacting with others in the LGBTQIA+ community.<br/><br/></p> <hr class="border-surface0"/> <h2 class="font-bold text-xl italic">How to play</h2> <p>Click on the locator button and start walking towards the icons on the map. Each icon is an important place
+        of queer history in Melbourne. When you arrive at the locations, a pop-up will tell you more. <br/><br/>
+        But what&#39;s that? Didn&#39;t we call this a game?<br/><br/> Click &#39;Play&#39; under the information and start a game with other PridePoints
+        players who have visited the same locations. Every time you complete a game, you will earn points and make your own
+        mark on the map. Slowly build up your own queer history through interactions with others in the community. <br/><br/></p> <hr class="border-surface0"/> <h2 class="font-bold italic text-xl">Queer quests</h2> <p>Don&#39;t feel like playing with others? That&#39;s okay! You can still earn points by visiting locations and
+        completing Queer Quests. These include GNSS enabled walking tours, collaborative story telling, contributing
+        to digital community artworks and more. Build up a reputation for yourself on the leaderboard or play anonymously.
+        It&#39;s your choice! We are just here to support you on your journey, whatever that looks like for you. <br/><br/></p> <hr class="border-surface0"/> <h2 class="font-bold italic text-xl">Content warning</h2> <p>We believe that Melbourne is for everyone, but that has not always been the case.
+        Homosexuality was illegal in our city until 1982 and until then, queer folks were forced to
+        live, meet and express themselves away from the public eye. This means that some of our locations are
+        places of violence and discrimination. If you find this triggering, use the &#39;NSFW&#39; button to
+        filter out sexually explicit or upsetting content. Notice something we didn&#39;t catch? Press report to help us
+        keep PridePoints safe for everyone.<br/><br/></p> <hr class="border-surface0"/> <h2 class="font-bold italic text-xl">Acknowledgements</h2> <p>This game is a celebration of progress and a testament to those who protested,
+        partied and pushed to change Melbourne into the inclusive city that it is today. PridePoints is a GNSS game adaptation of
+        <a href="https://www.queerways.au/" class="svelte-142pg1p">Queer-ways by LUCIANO and Georgia Keats</a>.
+        Created in 2021, Queer-ways is a Melbourne-based collaborative community arts
+        project that actualises the queer cartography of Australia by placing its history
+        onto interactive, digital maps. The information and data used by PridePoints
+        has been downloaded from Queer-ways and is a combination of information from the
+        <a href="https://queerarchives.org.au/" class="svelte-142pg1p">Australian Queer Archives (AQuA)</a> and community contributions.<br/><br/>
+
+        PridePoints is based on the stolen lands of the Wurundjeri Woi Wurrung people of the
+        Kulin Nations. We pay respect to Traditional Owners, Elders and Knowledge Holders of all
+        Indigenous nations and acknowledge the ongoing history of sexual and gender diversity represented within these cultures.</p>`;return{c(){e=s("div"),e.innerHTML=a,this.h()},l(t){e=l(t,"DIV",{class:!0,"data-svelte-h":!0}),u(e)!=="svelte-1c5klx9"&&(e.innerHTML=a),this.h()},h(){h(e,"class","w-full text-center p-6 text-text bg-base flex flex-col")},m(t,i){c(t,e,i)},p:o,i:o,o,d(t){t&&d(e)}}}class g extends p{constructor(e){super(),m(this,e,null,y,n,{})}}export{g as component};
