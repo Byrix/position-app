903f51b5f0535212aae3bdadb440e5828a0a4068
Sean <28848951+Byrix@users.noreply.github.com>
2024-10-27 02:30:06 +1100

    Updates

--- _app/immutable/nodes/3.CmYhU0OY.js
+++ _app/immutable/nodes/3.CmYhU0OY.js
@@ -1,11 +1,11 @@
-import{s as n,e as s,c as l,h as u,i as h,j as c,n as o,g as d}from"../chunks/scheduler.OQqrjfzc.js";import{S as p,i as m}from"../chunks/index.B3-jCvVV.js";function y(r){let e,a=`<p>New to Melbourne? Looking for community?
+import{s as n,e as s,c as l,h as u,i as h,j as c,n as o,g as d}from"../chunks/scheduler.DzfvL7Ic.js";import{S as p,i as y}from"../chunks/index.xHAgODIe.js";function m(r){let e,a=`<p>New to Melbourne? Looking for community?
         Or just interested in learning more about our city&#39;s queer history?
         PridePoints is the game for you! A GNSS game designed to help you explore
         Melbourne through a new lens while interacting with others in the LGBTQIA+ community.<br/><br/></p> <hr class="border-surface0"/> <h2 class="font-bold text-xl italic">How to play</h2> <p>Click on the locator button and start walking towards the icons on the map. Each icon is an important place
         of queer history in Melbourne. When you arrive at the locations, a pop-up will tell you more. <br/><br/>
         But what&#39;s that? Didn&#39;t we call this a game?<br/><br/> Click &#39;Play&#39; under the information and start a game with other PridePoints
         players who have visited the same locations. Every time you complete a game, you will earn points and make your own
-        mark on the map. Slowly build up your own queer history through interactions with others in the community. <br/><br/></p> <hr class="border-surface0"/> <h2 class="font-bold italic text-xl">Queer quests</h2> <p>Don&#39;t feel like playing with others? That&#39;s okay! You can still earn points by visiting locations and
+        mark on the map. Slowly build up your own queer history through interactions with others in the community. <br/><br/></p> <hr class="border-surface0"/> <h2 class="font-bold italic text-xl">QueerQuests</h2> <p>Don&#39;t feel like playing with others? That&#39;s okay! You can still earn points by visiting locations and
         completing Queer Quests. These include GNSS enabled walking tours, collaborative story telling, contributing
         to digital community artworks and more. Build up a reputation for yourself on the leaderboard or play anonymously.
         It&#39;s your choice! We are just here to support you on your journey, whatever that looks like for you. <br/><br/></p> <hr class="border-surface0"/> <h2 class="font-bold italic text-xl">Content warning</h2> <p>We believe that Melbourne is for everyone, but that has not always been the case.
@@ -24,4 +24,4 @@ import{s as n,e as s,c as l,h as u,i as h,j as c,n as o,g as d}from"../chunks/sc
 
         PridePoints is based on the stolen lands of the Wurundjeri Woi Wurrung people of the
         Kulin Nations. We pay respect to Traditional Owners, Elders and Knowledge Holders of all
-        Indigenous nations and acknowledge the ongoing history of sexual and gender diversity represented within these cultures.</p>`;return{c(){e=s("div"),e.innerHTML=a,this.h()},l(t){e=l(t,"DIV",{class:!0,"data-svelte-h":!0}),u(e)!=="svelte-1c5klx9"&&(e.innerHTML=a),this.h()},h(){h(e,"class","w-full text-center p-6 text-text bg-base flex flex-col")},m(t,i){c(t,e,i)},p:o,i:o,o,d(t){t&&d(e)}}}class g extends p{constructor(e){super(),m(this,e,null,y,n,{})}}export{g as component};
+        Indigenous nations and acknowledge the ongoing history of sexual and gender diversity represented within these cultures.</p>`;return{c(){e=s("div"),e.innerHTML=a,this.h()},l(t){e=l(t,"DIV",{class:!0,"data-svelte-h":!0}),u(e)!=="svelte-y5d6jv"&&(e.innerHTML=a),this.h()},h(){h(e,"class","w-full text-center p-6 text-text bg-base flex flex-col")},m(t,i){c(t,e,i)},p:o,i:o,o,d(t){t&&d(e)}}}class g extends p{constructor(e){super(),y(this,e,null,m,n,{})}}export{g as component};
